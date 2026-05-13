711150a10a20d3fc3e07c42afc1dde53cd236358
om tiwari <141153181+Omtiwari-code@users.noreply.github.com>
2026-05-13 20:01:57 +0530

    Added Band Name Generator Project

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Hello This is band name generator")
+colour=input("What is your favorite colour?\n")
+animal=input("What is your favorite animal?\n")
+print("Your Band name could be "+colour+" "+animal)
